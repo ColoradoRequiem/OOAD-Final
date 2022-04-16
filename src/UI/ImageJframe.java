40e2071e7d316c2d25316a8ff8772aebc2e4779e
Nare6880 <49699188+Nare6880@users.noreply.github.com>
2022-04-15 18:07:37 -0600

    Update ImageJframe.java

--- src/UI/ImageJframe.java
+++ src/UI/ImageJframe.java
@@ -2,7 +2,7 @@ package UI;
 
 import javax.swing.*;
 import java.awt.*;
-
+//https://java-demos.blogspot.com/2012/09/setting-background-image-in-jframe.html
 public class ImageJframe  extends JComponent {
     private Image background;
     public ImageJframe(Image image){
